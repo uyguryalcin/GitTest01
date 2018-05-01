975af119cad2b98b4c64713ccc5725fdd0b13110
uyguryalcin <uyguryalcin@gmail.com>
2018-05-01 21:59:14 +0300

    Initial commit
    Project initialized

---
+++ GitTest01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTest01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UYGUR BÜLENT YALÇIN !");
+        }
+    }
+}
